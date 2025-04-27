f584839b908a143fe31ecc2bd14fb1a5c86f875a
Ngoc Khanh <khanhk56a12@gmail.com>
2025-04-27 16:52:09 +0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,15 @@
+
+setup-monitor:
+	docker compose -f ./docker/docker-compose.yml up -d
+
+setup-edge-driver:
+	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+	sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge.list'
+	sudo apt update
+	sudo apt install microsoft-edge-stable
+
+run:
+	sitespeed.io https://kiosk-sit3.qa.spdigital.sg/payment \
+      --config config/config.json \
+      --slug myTest
---
+++ scripts/login.js
@@ -0,0 +1,21 @@
+module.exports = async function (context, commands) {
+
+    const redirectUrl = 'https://kiosk-sit3.qa.spdigital.sg/payment';
+
+    await commands.navigate(redirectUrl);
+    const currentUrl = await commands.js.run('return document.location.href');
+
+    if (currentUrl.includes('auth0')) {
+        context.log.info('Redirected to Auth0 login...');
+
+        await commands.addText.byId('quanpham@spgroup.com.sg', '1-email',)
+        await commands.addText.byId('Password@123', '1-password')
+
+        await commands.click.byId('1-submit');
+
+        context.log.info('Submitted login form.');
+        await commands.wait.byPageToComplete();
+    } else {
+        context.log.info('Already logged in, skipping login step.');
+    }
+};
